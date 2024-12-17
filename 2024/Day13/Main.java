e20255b9864e55ffa1828c278e76918b644b7474
Dmitrii Ivanushkin <dmitrii.unwritten183@passmail.net>
2024-12-17 01:20:09 +0100

    feat(2024): day 13

---
+++ 2024/Day13/Main.java
@@ -0,0 +1,15 @@
+package Day13;
+
+import Day13.Solver;
+
+public class Main {
+  public static void main(String[] args) {
+    Solver solver = new Solver();
+
+    String resultPartOne = solver.partOne();
+    System.out.println("Part One Result: " + resultPartOne);
+
+    String resultPartTwo = solver.partTwo();
+    System.out.println("Part Two Result: " + resultPartTwo);
+  }
+}
