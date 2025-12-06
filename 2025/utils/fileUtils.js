cf106f69c34be1e6bec097f37cecef127839c59d
Dmitrii Ivanushkin <dmitrii.unwritten183@passmail.net>
2025-12-06 20:22:10 +0100

    feat(2025): day 5

--- 2025/utils/fileUtils.js
+++ 2025/utils/fileUtils.js
@@ -3,7 +3,7 @@ import { readFile } from "fs/promises";
 export async function readLines(filename) {
   try {
     const data = await readFile(filename, "utf-8")
-    return data.split("\n").filter(l => l !== "")
+    return data.split("\n").filter((l, i, arr) => i !== arr.length - 1 || l !== "")
   } catch (err) {
     console.error("Error reading file:", err)
     return []
