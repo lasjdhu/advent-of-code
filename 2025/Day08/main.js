5182d94c3393eb955f3f0ee560798978ebe1a00e
Dmitrii Ivanushkin <dmitrii.unwritten183@passmail.net>
2025-12-23 21:09:45 +0100

    feat(2025): day 8

---
+++ 2025/Day08/main.js
@@ -0,0 +1,10 @@
+import { solveFirst, solveSecond } from "./solver.js";
+import { readLines } from "../utils/fileUtils.js";
+
+const lines = await readLines("2025/Day08/input.txt")
+
+const resultFirst = solveFirst(lines)
+console.log(resultFirst)
+
+const resultSecond = solveSecond(lines);
+console.log(resultSecond);
